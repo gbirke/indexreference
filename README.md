5aecee6568dbc6f5fed8ea90d4ad2e8bbe9b714f
Gabriel Birke <birke@d-scribe.de>
2013-01-16 16:57:53 +0100

    Added toolbar buttons

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ or an error message if the referenced counter was not found.
 
 ### Examples
 
-    For detailed numbers, see <idxref Tab. #1>
+    For detailed numbers, see <idxref Tab. #1>.
 
     As you can see in <idxref Fig. 4>, snowmen are nearly invisible in front of 
     white backgrounds.
---
+++ script.js
@@ -0,0 +1,79 @@
+/* 
+ * Toolbar button for indexreferences
+ */
+
+if(window.toolbar!=undefined){
+    toolbar[toolbar.length] = {
+        "type":"indexreferencepicker",
+        "title":"Index reference",
+        "icon":"../../plugins/indexreference/indexref_icon.png"
+    };
+}
+
+function createIndexreferencePicker(pickerid) {
+    var picker, btn, i, body;
+    picker               = document.createElement('div');
+    picker.className         = 'picker';
+    picker.id                = pickerid;
+    picker.style.position    = 'absolute';
+    picker.style.marginLeft  = '-10000px';
+    body = document.getElementsByTagName('body')[0];
+    body.appendChild(picker);
+    return picker;
+}
+
+function updateIndexreferencePicker(pickerid, edid) {
+    
+    function getInsertHandler(idxname, idxnum) {
+        return function(){  
+            insertTags(edid,'<idxref '+idxname.replace(/^\s*(.*?)\s*$/, "$1") + ' ' + idxnum + '>', '', '');
+            pickerClose();
+        };
+    }
+    
+    var picker, i, text, rx_indexnum, search_result;
+    picker = document.getElementById(pickerid);
+    while (picker.firstChild) {
+        picker.removeChild(picker.firstChild);
+    }
+    rx_indexnum = /<idxnum\s*([^#]+)#(\d+)([^>]*)>/g;
+    text = document.getElementById(edid).value;
+    i = 0;
+    while((search_result = rx_indexnum.exec(text)) !== null ) {
+        btn = document.createElement('button');
+        btn.className = 'pickerbutton';
+        btn.textContent = search_result[1]+" #"+search_result[2];
+        if(search_result[3]) {
+            btn.title = search_result[3];
+        }
+        addEvent(btn,'click', getInsertHandler(search_result[1], search_result[2]));
+        picker.appendChild(btn); 
+        i++;
+    }
+    if(!i) {
+        picker.textContent = "No index numbers";
+    }
+}
+
+/**
+ * Add button action for picker buttons and create picker element
+ *
+ * @param  DOMElement btn   Button element to add the action to 
+ * @param  array      props Associative array of button properties
+ * @param  string     edid  ID of the editor textarea
+ * @param  int        id    Unique number of the picker
+ * @return boolean    If button should be appended
+ * @author Gabriel Birke <gb@birke-software.de>
+ */
+function addBtnActionIndexreferencepicker(btn, props, edid, id)
+{
+    var pickerid = 'picker'+(pickercounter++);
+    createIndexreferencePicker(pickerid );
+    addEvent(btn, 'click', function(){
+        updateIndexreferencePicker(pickerid, edid)
+        pickerToggle(pickerid, btn);
+        return false;
+    });
+    return true;
+}
+
